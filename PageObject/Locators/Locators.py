5345db9b242194804460fa1ebe20f27a0776798a
Mateo Agudelo Garcia <mateo.agudelo80@eia.edu.co>
2025-03-18 12:08:16 -0500

    first commit

---
+++ PageObject/Locators/Locators.py
@@ -0,0 +1,13 @@
+# Login page selector
+login_username = "css:input[name='username']"
+login_password = "css:input[name='password']"
+login_submit = "css:button.orangehrm-login-button"
+
+# Top header selector
+title = "css:div.oxd-topbar-header-title h6"
+
+# Side panel selector
+side_panel_header = "css:div.oxd-sidepanel-header"
+side_panel_body = "css:div.oxd-sidepanel-body"
+view_pim_module = "css:a[href$='viewPimModule']"
+view_admin_module = "css:a[href$='viewAdminModule']"
